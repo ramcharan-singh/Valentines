b3b7e7966a753cbd6b1b3889c63371112014e292
ramcharan-singh <109129250+ramcharan-singh@users.noreply.github.com>
2025-01-30 16:03:22 +0000

    Just Trying

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,24 @@
 import streamlit as st
+import numpy as np
+
+# Set page configuration
+st.set_page_config(
+    page_title = 'Rosie Boo',
+    page_icon = '?',
+    layout = 'wide',
+    )
 
 st.title("🎈 My new app")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Hi Baby. Lets play a game of connections"
 )
+
+options = ["North", "East", "South", "West"]
+options2 = ["Wenny", "Rosie", "Madeline", "Divya"]
+selection = st.pills("Directions", options, selection_mode="multi", label_visibility="hidden")
+selection2 = st.pills("Names", options2, selection_mode="multi", label_visibility="collapsed")
+
+if st.button("Submit"):
+    st.markdown(f"Your selected options: {selection, selection2}.")
+
+st.markdown("![Alt Text](https://media.giphy.com/media/vFKqnCdLPNOKc/giphy.gif)")
